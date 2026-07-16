ffad291d064f1c546f9ea9816ed31c3f53a1d369
unknown <ahmedlaggoun@hotmail.com>
2026-07-16 19:46:37 +0100

    5 cmt

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // 1. Import Tailwind
 
 export default defineConfig({
+  base: '/np_landingpage/',
   plugins: [
     react(),
     tailwindcss(), // 2. Add Tailwind plugin here
